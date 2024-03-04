db699f4ea80c4a0de994b14c97cfb1c59b30eee0
Monsef75 <141649259+Monsef75@users.noreply.github.com>
2024-03-04 17:31:07 +0100

    t

--- project/src/app/Profile/page.tsx
+++ project/src/app/Profile/page.tsx
@@ -1,14 +1,60 @@
 import { getuserInfo } from "@/actions/FetchUserInfo"
 import Profile from "@/types/profile"
+import Avatar from '@/public/Avatar.png'
+import Image from "next/image"
 
-
-export default async function Page() {
-    const userInfo:Profile =  await getuserInfo()
-    return(<div>
+/*
+<div>
         {Object.entries(userInfo).map(([key, value]) => (
       <div key={key}>
        <b>{key}:</b> {value}
       </div>
     ))}
-    </div>)
+    </div>
+*/
+
+export default async function Page() {
+    const userInfo:Profile =  await getuserInfo()
+    return(
+      <div className="Box shadow m-4">
+        <div className="Profile pt-4">
+            <div className="Box d-flex align-items-center gap-3 p-3">
+                <article className="FullName">
+                <Image src={Avatar} width={120} height={120} alt="" className="rounded-circle" />
+                    <p className="c-text fw-bold s20">Lisha Lokwani</p>
+                    <p className="c-grey s15">Student</p>
+                </article>
+                <article className="OtherInfo d-flex flex-column gap-3 ms-5 flex-grow-1">
+                    <div className="Row">
+                        <div className="Text d-flex gap-3">
+                            <span >Place of birth</span>
+                            <span >Date of birth</span>
+                        </div>
+                        <div className="Inputs d-flex gap-3">
+                            <input type="text" className="Input px-2 py-2 rd-5 default" value="Ain Kercha" readonly />
+                            <input type="text" className="Input px-2 py-2 rd-5 default" value="2004-02-09" readonly />
+                        </div>
+                    </div>
+                    <div className="Row">
+                        <div className="Text d-flex gap-3">
+                            <span>Baccalaureate degree</span>
+                            <span>Baccalaureate specialty</span>
+                            <span>Baccalaureate year</span>
+                        </div>
+                        <div className="Inputs d-flex gap-3">
+                            <input type="text" className="Input px-2 py-2 rd-5 default" value="14.5" readonly />
+                            <input type="text" className="Input px-2 py-2 rd-5 default" value="SCIENCES EXPERIMENTALES" readonly />
+                            <input type="text" className="Input px-2 py-2 rd-5 default" value="2019" readonly />
+                        </div>
+                    </div>
+                </article>
+            </div>
+        </div>
+        <div className="ProfileId pt-4">
+            <p className="px-4 py-1 bc-prim-blue c-white fw-bold s18">matricule : 37049153</p>
+        </div>
+    </div>
+
+    )
   }
+  
